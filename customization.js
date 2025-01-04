269aa476095f3916ce180506b702200252eb9692
tanishbooti <tanishbooti@gmail.com>
2025-01-04 20:38:18 +0530

    Update customization.js

--- customization.js
+++ customization.js
@@ -3,8 +3,8 @@ window.selectedTime = null;
 
 
 function toggleClass(selectedButton, buttonGroup) {
-    buttonGroup.forEach(button => button.classList.remove('choose'));
-    selectedButton.classList.add('choose');
+    buttonGroup.forEach(button => button.classList.remove('choose')); //iterates through each btn in btngrp and removes choose class
+    selectedButton.classList.add('choose'); //selected btn ko choose class dedi
 }
 
 
@@ -14,7 +14,7 @@ timeButtons.forEach(button => {
         toggleClass(button, timeButtons);
         window.selectedTime = button.textContent; 
         
-        localStorage.setItem('selectedTime', window.selectedTime);
+        localStorage.setItem('selectedTime', window.selectedTime); //so that i can use in any other window
         checkSelections();
     });
 });
@@ -33,8 +33,8 @@ difficultyButtons.forEach(button => {
 
 
 const startButton = document.querySelector('.start');
-function checkSelections() {
-    const timeSelected = Array.from(timeButtons).some(button => button.classList.contains('choose'));
+function checkSelections() { //before pressing start btn, it identifies if any one of btns in each category is selected or not
+    const timeSelected = Array.from(timeButtons).some(button => button.classList.contains('choose')); 
     const difficultySelected = Array.from(difficultyButtons).some(button => button.classList.contains('choose'));
     if (timeSelected && difficultySelected) {
         startButton.style.display = 'block';
@@ -48,7 +48,7 @@ startButton.addEventListener('click', () => {
     disableButtons();
     startButton.style.display = 'none';
 
-    console.log("Selected Difficulty:", window.selectedDifficulty);
+    console.log("Selected Difficulty:",window.selectedDifficulty);
     console.log("Selected Time:", window.selectedTime);
 
     const messageContainer = document.querySelector('.message-container');
@@ -61,14 +61,14 @@ startButton.addEventListener('click', () => {
 
     const interval = setInterval(() => {
         countdown -= 1;
-        countdownElement.textContent = countdown;
+        countdownElement.textContent = countdown; //setting timer
 
         if (countdown === 0) {
             clearInterval(interval);
             document.body.innerHTML = '<h1 class="go">Go!</h1>';
 
             setTimeout(() => {
-                window.location.href = 'contest.html';
+                window.location.href = 'contest.html'; //after a short delay, it takes to contest.html
             }, 10);
         }
     }, 1000);
@@ -76,8 +76,8 @@ startButton.addEventListener('click', () => {
 
 
 function disableButtons() {
-    [...timeButtons, ...difficultyButtons].forEach(button => {
-        button.disabled = true;
-        button.classList.add('disabled');
+    [...timeButtons, ...difficultyButtons].forEach(button => { //taking 2 arrays at the time time, agr ... nhi use krte toh bs first element use hota
+        button.disabled = true; //setting disabled property true taki user koi change na kr paye
+        
     });
 }
